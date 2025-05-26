4ef2914c904f76f153d9f887ae8a77bb765a05e0
Leonny-CienciasdaComputacao <leobslopes@gmail.com>
2025-05-26 14:33:50 +0000

    Finalizei desafio novato

--- super_trunfo.c
+++ super_trunfo.c
@@ -50,15 +50,15 @@ número do estado, segundo número identifica cidade. */
     int populacao13, populacao23, populacao33, populacao43, populacao53, populacao63, populacao73, populacao83;
     int populacao14, populacao24, populacao34, populacao44, populacao54, populacao64, populacao74, populacao84;
     
-    int area11, area21, area31, area41, area51, area61, area71, area81;
+    float area11, area21, area31, area41, area51, area61, area71, area81;
     float area12, area22, area32, area42, area52, area62, area72, area82;
     float area13, area23, area33, area43, area53, area63, area73, area83;
     float area14, area24, area34, area44, area54, area64, area74, area84;
     
-    double pib11, pib21, pib31, pib41, pib51, pib61, pib71, pib81;
-    double pib12, pib22, pib32, pib42, pib52, pib62, pib72, pib82;
-    double pib13, pib23, pib33, pib43, pib53, pib63, pib73, pib83;
-    double pib14, pib24, pib34, pib44, pib54, pib64, pib74, pib84;
+    float pib11, pib21, pib31, pib41, pib51, pib61, pib71, pib81;
+    float pib12, pib22, pib32, pib42, pib52, pib62, pib72, pib82;
+    float pib13, pib23, pib33, pib43, pib53, pib63, pib73, pib83;
+    float pib14, pib24, pib34, pib44, pib54, pib64, pib74, pib84;
     
     int turistico11, turistico21, turistico31, turistico41, turistico51, turistico61, turistico71, turistico81;
     int turistico12, turistico22, turistico32, turistico42, turistico52, turistico62, turistico72, turistico82;
@@ -66,74 +66,78 @@ número do estado, segundo número identifica cidade. */
     int turistico14, turistico24, turistico34, turistico44, turistico54, turistico64, turistico74, turistico84;
 
 //Apresentação.
-printf("Bem-vindo ao jogo de cartas Super Trunfo Países!\nA seguir será solicitado a você o preenchimento dos dados das cartas.\n");
+    printf("Bem-vindo ao jogo de cartas Super Trunfo Países!\nA seguir será solicitado a você o preenchimento dos dados das cartas.\n");
 
 //Interação com usuário.
-printf("Iniciaremos agora...\n");
+    printf("Iniciaremos agora...\n");
 
-printf("Digite a letra que deseja utilizar para identificar o primeiro estado (A - H): \n");
-scanf("%c", &letra1);
+    printf("Digite a letra que deseja utilizar para identificar o primeiro estado (A - H): \n");
+        scanf("%c", &letra1);
 
-printf("Digite o nome deste estado: \n");
-scanf(" %s", &nomeest1);
+    printf("Digite o nome deste estado: \n");
+        scanf(" %49s", nomeest1);
 
 //Identificação primeira cidade.
-printf("Digite o número da primeira cidade, de %s (1 a 4): \n", nomeest1);
-scanf(" %d", &numero11);
+    printf("Digite o número da primeira cidade, de %s (1 a 4): \n", nomeest1);
+        scanf(" %d", &numero11);
 
-printf("Digite o nome da primeira cidade, de %s: \n", nomeest1);
-scanf(" %s", &nomecid11);
+    printf("Digite o nome da primeira cidade, de %s: \n", nomeest1);
+        scanf(" %49s", nomecid11);
 
 //Dados da primeira cidade
-printf("Digite a população de %s: \n", nomecid11);
-scanf(" %d", &populacao11);
+    printf("Digite a população de %s: \n", nomecid11);
+        scanf(" %d", &populacao11);
 
-printf("Digite a área de %s em Km2: \n", nomecid11);
-scanf(" %d", &area11);
+    printf("Digite a área de %s em Km2: \n", nomecid11);
+        scanf(" %f", &area11);
 
-printf("Digite o PIB de %s em reais: \n", nomecid11);
-scanf(" %f", &pib11);
+    printf("Digite o PIB de %s em reais: \n", nomecid11);
+        scanf(" %f", &pib11);
 
-printf("Digite o número de pontos turísticos em %s: \n", nomecid11);
-scanf(" %d", &turistico11);
+    printf("Digite o número de pontos turísticos em %s: \n", nomecid11);
+        scanf(" %d", &turistico11);
 
 //Identificação segunda cidade.
-printf("Digite o número da segunda cidade, de %s (1 a 4): \n", nomeest1);
-scanf(" %d", &numero12);
+    printf("Digite o número da segunda cidade, de %s (1 a 4): \n", nomeest1);
+        scanf(" %d", &numero12);
 
-printf("Digite o nome da segunda cidade, de %s: \n", nomeest1);
-scanf(" %s", &nomecid12);
+    printf("Digite o nome da segunda cidade, de %s: \n", nomeest1);
+        scanf(" %49s", nomecid12);
 
 //Dados da primeira cidade
-printf("Digite a população de %s: \n", nomecid12);
-scanf(" %d", &populacao12);
+    printf("Digite a população de %s: \n", nomecid12);
+        scanf(" %d", &populacao12);
 
-printf("Digite a área de %s em Km2: \n", nomecid12);
-scanf(" %d", &area12);
+    printf("Digite a área de %s em Km2: \n", nomecid12);
+        scanf(" %f", &area12);
 
-printf("Digite o PIB de %s em reais: \n", nomecid12);
-scanf(" %f", &pib12);
+    printf("Digite o PIB de %s em reais: \n", nomecid12);
+        scanf(" %f", &pib12);
 
-printf("Digite o número de pontos turísticos em %s: \n", nomecid12);
-scanf(" %d\n", &turistico12);
+    printf("Digite o número de pontos turísticos em %s: \n", nomecid12);
+        scanf(" %d\n", &turistico12);
+
+//DEFINIÇÃO DOS CÓDIGOS
+    codigo11[4] = letra1 + numero11;
+    codigo12[4] = letra1 + numero12;
 
 //PRIMEIRA CARTA
-printf("Código: %c%d\n", letra1, numero11);
-printf("Estado: %s\n", nomeest1);
-printf("Cidade: %s\n", nomecid11);
-printf("População: %d\n", populacao11);
-printf("Área em Km2: %d\n", area11);
-printf("PIB: %d\n", pib11);
-printf("Número de Pontos Turísticos: %d\n\n", turistico11);
+    printf("Código: %4s\n", codigo11);
+    printf("Estado: %s\n", nomeest1);
+    printf("Cidade: %s\n", nomecid11);
+    printf("População: %d\n", populacao11);
+    printf("Área em Km2: %f\n", area11);
+    printf("PIB: %f\n", pib11);
+    printf("Número de Pontos Turísticos: %d\n\n", turistico11);
 
 //SEGUNDA CARTA
-printf("Código: %c%d\n", letra1, numero12);
-printf("Estado: %s\n", nomeest1);
-printf("Cidade: %s\n", nomecid12);
-printf("População: %d\n", populacao12);
-printf("Área em Km2: %d\n", area12);
-printf("PIB: %d\n", pib12);
-printf("Número de Pontos Turísticos: %d\n", turistico12);
+    printf("Código: %4s\n", codigo12);
+    printf("Estado: %s\n", nomeest1);
+    printf("Cidade: %s\n", nomecid12);
+    printf("População: %d\n", populacao12);
+    printf("Área em Km2: %f\n", area12);
+    printf("PIB: %f\n", pib12);
+    printf("Número de Pontos Turísticos: %d\n", turistico12);
 
 
 
